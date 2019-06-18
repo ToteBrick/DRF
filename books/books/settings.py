584141be3636e9c776494737eb47d6588a3bfd9d
ToteBrick <402981948@qq.com>
2019-06-18 19:55:41 +0800

    全局分页设置

--- books/books/settings.py
+++ books/books/settings.py
@@ -130,5 +130,8 @@ REST_FRAMEWORK = {
         'user': '20/day'  # 登录用户
     },
     # 过滤字段配置
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    # 全局分页设置
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5  # 每页数目
 }
